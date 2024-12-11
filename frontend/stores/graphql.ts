52a727e054adad563de8138795b640e4904d48a4
Scott Fasone <scott181182@yahoo.com>
2024-12-10 19:28:28 -0500

    feat(frontend): added GraphQL endpoint to local storage

--- frontend/stores/graphql.ts
+++ frontend/stores/graphql.ts
@@ -2,11 +2,23 @@ import { defineStore } from "pinia";
 
 
 
+const ENDPOINT_KEY = "graphqlEndpoint";
+const DEFAULT_ENDPOINT = "https://east.cesmii.net/graphql";
+
 export const useGraphQLStore = defineStore("graphql", () => {
-  // Default Endpoint.
-  const endpoint = ref("https://east.cesmii.net/graphql");
+  let defaultEndpoint = DEFAULT_ENDPOINT;
+  if(typeof localStorage !== "undefined") {
+    const localEndpoint = localStorage.getItem(ENDPOINT_KEY);
+    if(localEndpoint) {
+      defaultEndpoint = localEndpoint;
+    } else {
+      localStorage.setItem(ENDPOINT_KEY, defaultEndpoint);
+    }
+  }
+  const endpoint = ref(defaultEndpoint);
 
   function setEndpoint(url: string) {
+    localStorage.setItem(ENDPOINT_KEY, url);
     endpoint.value = url;
   }
 
